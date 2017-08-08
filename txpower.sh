c3716906ff84cc7bbc50e0c43a27496d715aded1
Hotanya <hotanya.r@gmail.com>
2017-08-08 22:13:08 +1200

    added prompt for user to choose region+ interface

--- txpower.sh
+++ txpower.sh
@@ -1,11 +1,19 @@
-!#/bin/bash
+#!/bin/bash
 
-echo Your current region setting
-sudo iw reg get
-echo
+echo "Check tx power"
+sudo iwconfig 
 
-echo Setting region as BO
-sudo iw reg set BO
-echo
+echo "Please set a region: "
+read reg
 
-sudo iwconfig wlan0 
+echo "Setting reg as $reg"
+sudo iw reg set $reg
+
+echo -e "Please choose the wireless interface to use"
+read interface
+
+echo "Setting $interface to monitor mode"
+sudo airmon-ng start $interface
+
+echo "Check tx power"
+sudo iwconfig 
