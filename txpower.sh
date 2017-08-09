c8f7d866c444af14635ab052dd360a9877dcec11
Hotanya <hotanya.r@gmail.com>
2017-08-09 15:32:26 +1200

    added a check for correct input of region

--- txpower.sh
+++ txpower.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-echo "Check tx power"
+echo Check tx power
 sudo iwconfig 
 
 echo "Please set a region: "
-read reg
 
-echo "Setting reg as $reg"
-sudo iw reg set $reg
+while true
+do
+    read reg
+    # try the command, and catch its output:
+    output=$( sudo iw reg set "$reg" 2>&1 ) # 2>&1 redirects all output made on STDERR to STDOUT
+
+    if [ -z "$output" ] #-z returns true for given argument
+    then
+        # output is empty - exit loop
+        break
+    else # output is not empty
+        echo "Please set a valid region."
+    fi
+
+done
+
 
 echo -e "Please choose the wireless interface to use"
 read interface
